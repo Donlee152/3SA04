1c7d7a39a2ce75091f1b72cf83ab6693f7c175fc
Donlee152/3SA03 <6110110152@psu.ac.th>
2020-09-04 02:29:41 +0700

    ปรับ Flex box

--- components/Weather.js
+++ components/Weather.js
@@ -39,6 +39,7 @@
     const styles = StyleSheet.create({
         backdrop: {
             flexDirection: 'column',
+            justifyContent: 'center',
             alignItems: 'center',
             width: '100%',
             height: '100%'
